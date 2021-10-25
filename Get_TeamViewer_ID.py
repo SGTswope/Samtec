1183c219d03ee6c4e8bd5d208e0650dbc37e9d94
Jordan Smith <jordansmith@Jordans-MacBook-Pro.local>
2021-10-25 17:08:29 -0400

    Added some try/except blocks

--- Get_TeamViewer_ID.py
+++ Get_TeamViewer_ID.py
@@ -11,21 +11,29 @@ def GetTV_ID():
 
     root = winreg.ConnectRegistry(ComputerName, HKEY_LOCAL_MACHINE)
 
-    root_key = winreg.OpenKey(root, r'SOFTWARE\TeamViewer', 0, access=winreg.KEY_READ)
-    Pathname = winreg.QueryValueEx(root_key, "ClientID")
+    try:
+        root_key = winreg.OpenKey(root, r'SOFTWARE\TeamViewer', 0, access=winreg.KEY_READ)
+        Pathname = winreg.QueryValueEx(root_key, "ClientID")
+        print('\n\nFrom :  HKLM\SOFTWARE\TeamViewer\n'+Pathname)
+    except:
+        print('\n\nNo ID at HKLM\SOFTWARE\TeamViewer')
 
-    root_key2 = winreg.OpenKey(root, r'SOFTWARE\WOW6432Node\TeamViewer', 0, access=winreg.KEY_READ)
-    Pathname2 = winreg.QueryValueEx(root_key2, "ClientID")
+    try:
+        root_key2 = winreg.OpenKey(root, r'SOFTWARE\WOW6432Node\TeamViewer', 0, access=winreg.KEY_READ)
+        Pathname2 = winreg.QueryValueEx(root_key2, "ClientID")
+        print('\n\nFrom :  HKLM\SOFTWARE\WOW6432Node\TeamViewer\n'+Pathname)
+    except:
+        print('\n\nNo ID at HKLM\SOFTWARE\WOW6432Node\TeamViewer')
 
-    print()
-    print()
-    print("From :  HKLM\SOFTWARE\TeamViewer")
-    print(Pathname)
-    print()
-    print("From :  HKLM\SOFTWARE\WOW6432Node\TeamViewer")
-    print(Pathname2)
-    print()
-    print()
+    #print()
+    #print()
+    #print("From :  HKLM\SOFTWARE\TeamViewer")
+    #print(Pathname)
+    #print()
+    #print("From :  HKLM\SOFTWARE\WOW6432Node\TeamViewer")
+    #print(Pathname2)
+    #print()
+    #print()
 
 
 GetTV_ID()
