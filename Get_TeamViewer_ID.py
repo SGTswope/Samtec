b6f32697722f20aa70fc162988b4167caca9b84c
SGTswope <sgtswope@gmail.com>
2021-10-25 17:00:56 -0400

    Update Get_TeamViewer_ID.py

--- Get_TeamViewer_ID.py
+++ Get_TeamViewer_ID.py
@@ -1,6 +1,9 @@
 from winreg import HKEY_LOCAL_MACHINE
 import winreg
 
+print()
+print()
+print()
 
 def GetTV_ID():
 
@@ -9,27 +12,20 @@ def GetTV_ID():
     root = winreg.ConnectRegistry(ComputerName, HKEY_LOCAL_MACHINE)
 
     root_key = winreg.OpenKey(root, r'SOFTWARE\TeamViewer', 0, access=winreg.KEY_READ)
+    Pathname = winreg.QueryValueEx(root_key, "ClientID")
+
     root_key2 = winreg.OpenKey(root, r'SOFTWARE\WOW6432Node\TeamViewer', 0, access=winreg.KEY_READ)
-
-
-    try:
-        Pathname = winreg.QueryValueEx(root_key, "ClientID")
-
-    except:
-        Pathname = winreg.QueryValueEx(root_key2, "ClientID")
-        pass
-
-    # Pathname2 = winreg.QueryValueEx(root_key2, "ClientID")
+    Pathname2 = winreg.QueryValueEx(root_key2, "ClientID")
 
     print()
     print()
     print("From :  HKLM\SOFTWARE\TeamViewer")
     print(Pathname)
     print()
-    # print("From :  HKLM\SOFTWARE\WOW6432Node\TeamViewer")
-    # print(Pathname2)
-    # print()
-    # print()
+    print("From :  HKLM\SOFTWARE\WOW6432Node\TeamViewer")
+    print(Pathname2)
+    print()
+    print()
 
 
 GetTV_ID()
